107acfdac998e666525e638bc8437675700e848e
John-D-Boom <johndboom@gmail.com>
2023-01-24 20:18:42 +0000

    whoops the last commit was supposed to have these

--- exercise_2_1.py
+++ exercise_2_1.py
@@ -6,7 +6,7 @@ import torchaudio
 
 
 root_dir = '/rds/user/jdb206/hpc-work/MLMI2/exp'
-file = 'test.json'
+file = 'train.json'
 
 # open the file and read in the contents
 with open(os.path.join(root_dir, file)) as json_file:
@@ -25,7 +25,36 @@ for key, value in data.items():
 
     #Load waveform, generate fbank, and save to fbanks/[spk_id]/fbank.pt
     waveform, sample_rate = torchaudio.load(wav_path)
-    fbank = torchaudio.compliance.kaldi.fbank(waveform) #default sample_freq is 16khz, same as TIMIT
+
+    # speedup_transform = torchaudio.prototype.transforms.speed(sample_rate, 1.1)
+    # slowdown_transform = torchaudio.prototype.transforms.speed(sample_rate, 0.9)
+    # fast_wav = speedup_transform(waveform, len(waveform))
+    # slow_wav = slowdown_transform(waveform, len(waveform))
+
+    slow_effects = [
+        ["speed", "0.9"],   # reduce the speed
+                            # This only changes sample rate, so it is necessary to
+                            # add `rate` effect with original sample rate after this.
+        ["rate", f"{sample_rate}"],
+    ]
+    fast_effects = [
+        ["speed", "1.1"],   # increase the speed
+                            # This only changes sample rate, so it is necessary to
+                            # add `rate` effect with original sample rate after this.
+        ["rate", f"{sample_rate}"],
+    ]
+
+    # Apply effects
+    waveform_slow, sample_rate_slow = torchaudio.sox_effects.apply_effects_tensor(
+        waveform, sample_rate, slow_effects)
+    
+    waveform_fast, sample_rate_fast = torchaudio.sox_effects.apply_effects_tensor(
+        waveform, sample_rate, fast_effects)
+
+    fbank_slow = torchaudio.compliance.kaldi.fbank(waveform_slow, sample_frequency= sample_rate_slow)
+    fbank_fast = torchaudio.compliance.kaldi.fbank(waveform_fast, sample_frequency= sample_rate_fast)
+
+    # fbank = torchaudio.compliance.kaldi.fbank(waveform) #default sample_freq is 16khz, same as TIMIT
     
     _ , entry = key.split('_')
     
@@ -39,7 +68,11 @@ for key, value in data.items():
         os.mkdir(entry_path)
     
     fbank_save_path = os.path.join(entry_path, 'fbank.pt')
-    torch.save(fbank, fbank_save_path)
+    slow_fbank_save_path = os.path.join(entry_path, 'fbank_0_9.pt')
+    fast_fbank_save_path = os.path.join(entry_path, 'fbank_1_1.pt')
+    torch.save(fbank_slow, slow_fbank_save_path)
+    torch.save(fbank_fast, fast_fbank_save_path)
+
 
     #Make the dictionary using same key as before
 
@@ -48,11 +81,14 @@ for key, value in data.items():
     fbank_dict["duration"] = dur
     fbank_dict["spk_id"] = spk_id
     fbank_dict["phn"] = phn
+    fbank_dict["fbank_0_9"] = slow_fbank_save_path
+    fbank_dict["fbank_1_1"] = fast_fbank_save_path
+
 
     output_json_dict[key] = fbank_dict
     # print(value["phn"])
     
 
-output_json = os.path.join(root_dir, 'test_fbank.json')
+output_json = os.path.join(root_dir, 'train_fbank_speeds.json')
 with open(output_json, 'w') as json_file:
     json.dump(output_json_dict, json_file, indent=4)
