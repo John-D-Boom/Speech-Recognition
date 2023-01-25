82b85a042964806de159a648fa716c5d74815845
John-D-Boom <johndboom@gmail.com>
2023-01-25 15:48:18 +0000

    amended the models so that bidirectional works again.
    logs for width + depth

--- models.py
+++ models.py
@@ -20,6 +20,31 @@ class BiLSTM_Dropout(nn.Module):
         self.lstm = nn.LSTM(in_dims, 
                             hidden_dims, 
                             num_layers, 
+                            bidirectional=True, 
+                            dropout = dropout) #This dropout only applies between LSTM layers
+
+        feed_forward_dropout = 0 #Default to 0 so that dropout only between LSTM layers
+        if num_layers == 1: #If only a single LSTM, dropout needs to actaully occur
+            feed_forward_dropout = dropout 
+
+        self.drop = nn.Dropout(p = feed_forward_dropout) 
+        self.proj = nn.Linear(hidden_dims *2 , out_dims)
+        self.num_layers = num_layers
+
+    def forward(self, feat):
+        hidden, _ = self.lstm(feat)
+        dropped = self.drop(hidden)
+        output = self.proj(dropped)
+        return output
+
+
+class BiLSTM_Dropout_Unidir(nn.Module):
+    def __init__(self, num_layers, in_dims, hidden_dims, out_dims, dropout):
+        super().__init__()
+
+        self.lstm = nn.LSTM(in_dims, 
+                            hidden_dims, 
+                            num_layers, 
                             bidirectional=False, 
                             dropout = dropout) #This dropout only applies between LSTM layers
 
